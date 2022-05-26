ba50398ac42aeb3b89ed80381d6569c844552bff
wayofwade <982034273@qq.com>
2022-05-26 23:13:23 +0800

    feat: 设置空标题;

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,5 +1,5 @@
 export default definePageConfig({
-  navigationBarTitleText: '',
+  navigationBarTitleText: '', // 设置为空就是没有标题，为了兼容支付宝
   "defaultTitle":"关于我们", 
   "transparentTitle": "auto" // 支付宝配置
 })
